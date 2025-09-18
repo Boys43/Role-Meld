b9ba15b48fa99ccaf1a783dac3431e79c12ecb8f
Nouman <nt50616849@gmail.com>
2025-09-18 14:11:01 +0500

    cors fixed

--- server/server.js
+++ server/server.js
@@ -13,9 +13,11 @@ const app = express();
 app.use(cookieParser())
 app.use(express.json())
 const allowedOrigins = [
-    "http://localhost:5173",
-    "https://role-meld-1.onrender.com",  // add more origins here
+    "http://localhost:5173",           // local dev
+    "https://role-meld.onrender.com",  // backend (API)
+    "https://role-meld-1.onrender.com" // frontend
 ];
+
 
 app.use(
     cors({
