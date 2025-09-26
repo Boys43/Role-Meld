c46f33b3c4d84d569a64186120f738a8d01c31eb
Nouman <nt50616849@gmail.com>
2025-09-26 20:55:39 +0500

    Backend Verification Done

--- server/models/authModels.js
+++ server/models/authModels.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const authSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    role: {type: String, enum: ['user', 'recruiter'], default: 'user'},
-    isAdmin: {type: Boolean, default: false},
-}, {timestamps: true});
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'recruiter'], default: 'user' },
+    isAdmin: { type: Boolean, default: false },
+    isVerified: { type: Boolean, default: false },
+    verificationOTP: { type: Number, default: 0 },
+}, { timestamps: true });
 
 const authModel = mongoose.model("User", authSchema);
 export default authModel;
