cf8c8007b9ed282c497580d8d907120ab1e3f605
Nouman <nt50616849@gmail.com>
2025-09-26 22:33:30 +0500

    UserController Fixed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,8 +4,13 @@ import LoginModel from '../components/LoginModel'
 const Login = () => {
 
   return (
-    <main className="w-full py-10 flex justify-center items-center">
+    <main className="h-screen overflow-hidden w-full flex justify-center items-center">
+      <div className="w-1/2 py-10 overflow-y-auto">
         <LoginModel />
+      </div>
+      <div className="border border-l-3 border-[var(--primary-color)] w-1/2">
+        <img src={assets.register_side} alt="Register Side" />
+      </div>
     </main>
   )
 }
