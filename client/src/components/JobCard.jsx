1e63f1dd0571300b22c94ac9a3e1c7b070ef786c
Nouman <nt50616849@gmail.com>
2025-09-27 07:52:57 +0500

    Find Jobs Loading Added

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -10,6 +10,7 @@ import { CiBookmark } from "react-icons/ci";
 import { FaBookmark } from "react-icons/fa";
 import { MdCancel } from 'react-icons/md';
 import LoginModel from './LoginModel';
+import { FaChevronRight } from "react-icons/fa";
 
 const JobCard = ({ e }) => {
     const { backendUrl, isLoggedIn, userData, toggleSaveJob, savedJobs } = useContext(AppContext);
@@ -36,7 +37,7 @@ const JobCard = ({ e }) => {
     
     return (
         <>
-            <li className='p-6 border relative border-gray-300 rounded-lg hover:shadow-lg transition-shadow flex flex-col gap-4 cursor-pointer'>
+            <li className='p-3 border relative border-gray-300 rounded-lg hover:shadow-lg transition-shadow flex flex-col gap-5 cursor-pointer'>
                 {/* Company Info */}
                 <div className='flex items-center gap-3'>
                     <img
@@ -48,22 +49,22 @@ const JobCard = ({ e }) => {
                 </div>
 
                 {/* Job Title */}
-                <h2 className='font-bold text-xl text-gray-900'>{e?.title}</h2>
+                <h3 className='font-bold text-xl text-gray-900'>{e?.title}</h3>
 
                 {/* Job Badges */}
                 <div className='flex flex-wrap items-center gap-2'>
-                    <span className='px-3 py-1 text-sm rounded-full bg-blue-500 text-white font-medium'>{e?.jobType}</span>
-                    <span className='px-3 py-1 text-sm rounded-full bg-red-500 text-white font-medium'>{e?.locationType}</span>
-                    <span className='px-3 py-1 text-sm rounded-full bg-green-500 text-white font-medium'>{e?.category}</span>
+                    <span className='px-3 text-xs rounded bg-blue-300 border border-blue-500 text-white font-medium'>{e?.jobType}</span>
+                    <span className='px-3 text-xs rounded bg-red-300 border border-red-500 text-white font-medium'>{e?.locationType}</span>
+                    <span className='px-3 text-xs rounded bg-green-300 border border-green-500 text-white font-medium'>{e?.category}</span>
                 </div>
 
                 {/* Salary */}
-                <div className='text-gray-700 font-medium px-3 py-1 rounded border border-gray-500 bg-gray-300 w-max'>
+                <div className='text-gray-700 font-medium px-2 rounded border border-gray-500 bg-gray-300 w-max'>
                     {e?.salary} $
                 </div>
 
                 {/* Buttons */}
-                <div className='flex gap-4 mt-2'>
+                <div className='flex gap-4 '>
                     <button
                         disabled={userData?.appliedJobs?.includes(e?._id)}
                         onClick={() => {
@@ -74,15 +75,14 @@ const JobCard = ({ e }) => {
                                 setLoginReminder(true)
                             }
                         }}
-
                     >
                         Apply Now
                     </button>
-                    <button className='flex-1 bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 rounded transition'
+                    <span className='flex gap-2 items-center relative font-semibold rounded transition after:w-0 hover:after:w-full after:h-[2px] after:bg-[var(--primary-color)] after:rounded-full after:transition-all after:duration-300  cursor-pointer after:left-0 after:absolute after:bottom-0'
                         onClick={() => navigate(`/jobdetails/` + e?._id)}
                     >
-                        View Details
-                    </button>
+                        View Details <FaChevronRight size={15} />
+                    </span>
                 </div>
                 <span
                     onClick={() => toggleSaveJob(e?._id)}
